2ee471aedeb4d3fa4c3f7fbdcd5c64271fb9a64d
William <euwilliamsantana@gmail.com>
2025-08-31 19:38:28 -0300

    feat: Layout header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${fontRoboto.variable}  antialiased`}>{children}</body>
+    <html lang="pt-br">
+      <body className={`${fontRoboto.variable} min-h-screen  antialiased`}>
+        <header className="border-b border-black/10 bg-white">
+          <div className="mx-auto max-w-6xl px-4 py-4 flex items-center justify-between">
+            <h1 className="text-lg font-semibold">Crystal 💎</h1>
+            <button className="text-sm bg-cyan-600 px-4 py-2 border-none rounded text-white">
+              Faça uma simulação agora!
+            </button>
+          </div>
+        </header>
+        <main className="mx-auto max-w-6xl px-4 py-6">{children}</main>
+      </body>
     </html>
   )
 }
